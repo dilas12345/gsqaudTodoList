b69166a1fcbb72d683a7f607487f829dd6ccf908
che <josephdilas@gmail.com>
2021-06-04 00:56:01 +0100

    component done with screens done halfway

---
+++ src/screens/ScreenTodoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    Image,
+} from 'react-native';
+import { themeable } from '../themes';
+import TodoList from './TodoList';
+
+function Main() {
+    const {mainBgImgStyle, mainBgImgSrc} = this.props
+    return (
+        <Image>
+            
+        </Image>
+    )
+}
---
+++ src/screens/TodoList.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {
+    Image,
+} from 'react-native';
+import { themeable } from '../themes';
+import TodoList from './TodoList';
